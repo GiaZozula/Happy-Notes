13419feeb89d86c703ca181de755e858373e776a
olstinker <giazozula@gmail.com>
2022-12-03 11:38:05 -0500

    slowly working through this project, with the bookshelf code-along as a guide

--- src/App.js
+++ src/App.js
@@ -1,11 +1,57 @@
 import './App.css';
+import firebase from './firebase.js';
+import {useEffect, useState} from 'react';
+import {getDatabase, ref, onValue} from 'firebase/database';
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  // the useEffect hook is used to request the data from firebase
+  useEffect(() => {
+    // variable that holds database content
+    const database = getDatabase(firebase)
+
+    // variable that references the database
+    const dbRef = ref(database)
+
+    onValue(dbRef, (response) => {
+      // variable that stores the new state
+      const newState = [];
+
+      // stores the response from firebase
+      // val() is a firebase method
+      const data = response.val();
+
+      // for loop to access each individual item in the data object
+      for (let key in data) {
+        // inside the loop, we push each book name to the newState array in the onValue function
+        newState.push(data[key]);
+      }
+
+      // then, we call setItems to update the component's state using the local array newState
+      setItems(newState);
+
+    })
+    // using an empty dependancy array in order to render this code only once after initializing 
+  }, [])
+
+  // JSX
   return (
     <div className="App">
       <header>
         <h1>Pinboard App!</h1>
       </header>
+      <ul>
+        {/* map through the items array, displaying each item */}
+        {items.map((item) => {
+          return (
+            <li>
+              <p>{item}</p>
+            </li>
+          )
+        })}
+      </ul>
+
     </div>
   );
 }
@@ -13,4 +59,15 @@ function App() {
 export default App;
 
 
-// 
+// PSEUDO CODE for MVP
+
+// Create a state to store Firebase data (items)
+// Create a state to store user's input data (userInput)
+// Use an event object (from onChange or something) attached to an input of some kind to track the user's input and store it in the userInput state (will likely use a button so that the function is only called on submit)
+// On change is listening to input (as soon as they type even a single letter), and then On submit, theres even more one
+
+// On value tiggers once on load, and then it will also update the database
+
+// Bind the HTML value attribute of the input to the component's state
+// Call the function on submit and at that point access/update the database 
+// Render everything to the page, ideally styled as post-it notes or something similar
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlJxQPSFOUUCGgEdFZ_BhGBaXuEy-I5xw",
+  authDomain: "project-three-18c16.firebaseapp.com",
+  projectId: "project-three-18c16",
+  storageBucket: "project-three-18c16.appspot.com",
+  messagingSenderId: "707037360118",
+  appId: "1:707037360118:web:0a4673e95b38f3024521d7"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+// this exports the CONFIGURED version of firebase
+export default firebase;
